65f8ef5b0676dc3b5d49149ea503108b8a87b55a
kmyounes <kmyounes@protonmail.com>
2016-12-11 09:49:26 +0100

    renommé :         script.sh -> mraya.sh

--- mraya.sh
+++ mraya.sh
@@ -9,11 +9,12 @@ read -p 'veuillez entrer votre choix : ' choix
 if [ $choix = 1 ]
 then 
                     cd /etc/apt && sudo mv sources.list sources.list.original && sudo wget http://mirror/sources.list.xenial/sources.list
-
+                    sudo apt update
 
                     elif [ $choix=2 ] 
 then
 cd /etc/apt && sudo mv sources.list.original sources.list
+            sudo apt update
          else echo veuillez entrer un numero valide 
          fi
 
